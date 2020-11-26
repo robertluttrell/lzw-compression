a041ae61cfd6715d37d798b9299b10635879c8c8
Robert Luttrell <robert.luttrell2@gmail.com>
2020-11-26 13:31:58 -0800

    Implemented Compressor.initializeTable()

--- src/Compressor.java
+++ src/Compressor.java
@@ -1,12 +1,34 @@
 import java.nio.file.Files;
+import java.util.HashMap;
 
 public class Compressor
 {
     private byte[] bytes;
+    private int tableSize;
+    private HashMap<String, Integer> table;
 
-    public Compressor(byte[] bytes)
+    public Compressor(byte[] bytes, int tableSize)
     {
         this.bytes = bytes;
+        this.tableSize = tableSize;
+        initializeTable();
     }
+
+    private void initializeTable()
+    {
+        table = new HashMap<>();
+        for (int i = 0; i <= 255; i++)
+        {
+            char c = (char) i;
+            table.put(String.valueOf(c), i);
+        }
+    }
+
+    public void compress()
+    {
+
+    }
+
+
 
 }
