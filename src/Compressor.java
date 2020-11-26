48ebe6f34e27cc8b1bbeb8f54c87f5c454f601f9
Robert Luttrell <robert.luttrell2@gmail.com>
2020-11-26 15:11:43 -0800

    Implemented Compressor.compress()

--- src/Compressor.java
+++ src/Compressor.java
@@ -1,17 +1,22 @@
-import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Compressor
 {
-    private byte[] bytes;
+    private String input;
+    private ArrayList<Integer> output;
     private int tableSize;
     private HashMap<String, Integer> table;
+    private int nextCode;
 
-    public Compressor(byte[] bytes, int tableSize)
+    public Compressor(String input, int tableSize)
     {
-        this.bytes = bytes;
+        this.input = input;
         this.tableSize = tableSize;
+        this.output = new ArrayList<>();
         initializeTable();
+        this.nextCode = 256;
     }
 
     private void initializeTable()
@@ -26,7 +31,44 @@ public class Compressor
 
     public void compress()
     {
+        String s = "";
+        int i = 0;
+        String ch;
 
+        while (i < input.length())
+        {
+            ch = String.valueOf(input.charAt(i));
+            if (table.containsKey(s + ch))
+                s = s + ch;
+            else
+            {
+                output.add(table.get(s));
+                table.put(s + ch, nextCode);
+                s = ch;
+                nextCode++;
+            }
+        }
+        output.add(table.get(s));
+        /*
+        String p = String.valueOf(input.charAt(0));
+        String c = "";
+
+        for (int i = 1; i < input.length(); i++)
+        {
+            c = String.valueOf(input.charAt(i));
+
+            if (table.containsKey(p + c))
+                p = p + c;
+            else
+            {
+                output.add(table.get(p));
+                table.put(p + c, nextCode);
+                p = c;
+            }
+            nextCode++;
+        }
+        output.add(table.get(p));
+         */
     }
 
 
--- test/CompressorTests.java
+++ test/CompressorTests.java
@@ -3,5 +3,12 @@ import org.junit.Assert;
 
 public class CompressorTests
 {
-
+    @Test
+    public void test01()
+    {
+        String s = "BABAABAAA";
+        Compressor c = new Compressor(s, 4096);
+        c.compress();
+        System.out.println("");
+    }
 }
