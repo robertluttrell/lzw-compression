3f35ae1a5120a4232e943d2e300c4a77e3b55f32
Robert Luttrell <robert.luttrell2@gmail.com>
2020-11-26 18:46:37 -0800

    Finished argparse for mode and files

--- src/LZW.java
+++ src/LZW.java
@@ -4,11 +4,27 @@ import org.apache.commons.cli.*;
 public class LZW
 {
 
+    private static void compressFiles(String[] fileNames)
+    {
+        System.out.println("Compress:");
+        for (String s : fileNames)
+            System.out.println(s);
+        /* TODO: iterative call to Compressor.compress(). Print stats */
+    }
+
+    private static void decompressFiles(String[] fileNames)
+    {
+        System.out.println("Decompress:");
+        for (String s : fileNames)
+            System.out.println(s);
+        /* TODO: iterative call to Decompressor.decompress() */
+    }
+
     public static void main(String[] args)
     {
         CommandLineParser parser = new DefaultParser();
         Options options = new Options();
-        Option mode = Option.builder("mode")
+        Option modeOption = Option.builder("mode")
                 .argName("mode")
                 .desc("Set the mode to either compress or decompress")
                 .hasArg()
@@ -22,21 +38,27 @@ public class LZW
                 .required()
                 .build();
 
-        options.addOption(mode);
+        options.addOption(modeOption);
         options.addOption(inputFiles);
         HelpFormatter formatter = new HelpFormatter();
 
         try
         {
             CommandLine line = parser.parse(options, args);
-            if (!(line.getOptionValue("mode").equals("compress") || line.getOptionValue("mode").equals("decompress")))
+            String mode = line.getOptionValue("mode");
+            String[] files = line.getOptionValues("files");
+
+            if (mode.equals("compress"))
+                compressFiles(files);
+            else if (mode.equals("decompress"))
+                decompressFiles(files);
+            else
                 formatter.printHelp("lzw", options);
         }
         catch (ParseException e)
         {
+            e.printStackTrace();
             formatter.printHelp("lzw", options);
         }
-
-
     }
 }
