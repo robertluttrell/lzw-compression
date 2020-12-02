9bd7fa1d3f23df43d241db3338399620f8969ef6
Robert Luttrell <robert.luttrell2@gmail.com>
2020-12-01 17:21:41 -0800

    Started implementing bit packing

--- src/Compressor.java
+++ src/Compressor.java
@@ -1,3 +1,5 @@
+import jdk.jfr.Unsigned;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -5,16 +7,21 @@ import java.util.List;
 public class Compressor
 {
     private String input;
-    private ArrayList<Integer> output;
+    private int buffer;
+    private int numBits;
+    private ArrayList<Byte> output;
     private int maxTableSize;
     private HashMap<String, Integer> table;
     private int nextCode;
+    private int numBitsInBuffer;
 
     public Compressor(String input, int maxTableSize)
     {
         this.input = input;
         this.maxTableSize = maxTableSize;
         this.output = new ArrayList<>();
+        this.buffer = 0;
+        this.numBitsInBuffer = 0;
     }
 
     private void initializeTable()
@@ -26,6 +33,31 @@ public class Compressor
             table.put(String.valueOf(c), i);
         }
         nextCode = 256;
+        numBits = 9;
+    }
+
+    private void writeByteFromBuffer()
+    {
+        int byteInLSB = buffer >> (numBitsInBuffer - 8);
+        int mask;
+
+    }
+
+    private void addCodeToBuffer(int code)
+    {
+        buffer <<= numBits;
+        buffer += code;
+        numBitsInBuffer += numBits;
+    }
+
+
+    private void addCodeToOutput(int code)
+    {
+        while (numBitsInBuffer > 8)
+            writeByteFromBuffer();
+
+        addCodeToBuffer(code);
+
     }
 
     public void compress()
@@ -41,6 +73,9 @@ public class Compressor
             if (table.size() >= maxTableSize)
                 initializeTable();
 
+            if (nextCode > Math.pow(2.0, numBits))
+                numBits += 1;
+
             ch = String.valueOf(input.charAt(i));
             if (ch.equals("")) {
                 i++;
@@ -50,15 +85,15 @@ public class Compressor
                 s = s + ch;
             else
             {
-                output.add(table.get(s));
+                addCodeToOutput(table.get(s));
                 table.put(s + ch, nextCode);
                 s = ch;
                 nextCode++;
             }
             i++;
         }
-        output.add(table.get(s));
+        addCodeToOutput(table.get(s));
     }
 
-    public List<Integer> getOutput() { return this.output; }
+    public List<Byte> getOutput() { return this.output; }
 }
